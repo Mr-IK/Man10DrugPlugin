fc1be3daae8eb7c3cd7dd761e20b36bd2f65e7e1
forest611 <forest611328@gmail.com>
2018-09-15 11:11:32 +0900

    first commit

---
+++ src/main/java/red/man10/man10drugplugin/Man10DrugPlugin.java
@@ -0,0 +1,17 @@
+package red.man10.man10drugplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Man10DrugPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
