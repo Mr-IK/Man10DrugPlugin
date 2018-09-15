16437b14adbfc7c08c34950ea5169d7396683e8e
forest611 <forest611328@gmail.com>
2018-09-15 17:29:24 +0900

    config読み込み部分 リファクタリング

---
+++ src/main/java/red/man10/man10drugplugin/LoadConfigData.java
@@ -0,0 +1,68 @@
+package red.man10.man10drugplugin;
+
+import org.bukkit.Bukkit;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+/*
+drugName...拡張子を含まないファイル名
+ */
+
+public class LoadConfigData {
+    public static void LoadConfig(String drugName,String brLine){//str...行
+        if (brLine ==null){
+            Bukkit.getLogger().info("空白の行があります");
+            return;
+        }
+        DrugData data = loadData(drugName);
+        String[] str = brLine.split(":");
+        switch (str[0]){
+            case "NAME":data.name = str[1];break;
+            case "ID":data.id = Integer.parseInt(str[1]);break;
+            case "LEVEL":data.level = Integer.parseInt(str[1]);break;
+            case "COUNT":data.count = Integer.parseInt(str[1]);break;
+            case "BUFF":{
+                String[] buffLevel = str[1].split("/");
+                for (int i = 0;i!=buffLevel.length;i++){
+                    String[] buff = buffLevel[i].split(",");
+                    data.buffs.put(drugName+i+1,buff);//level分けようにナンバーをふる
+                }
+                break;
+            }
+            case "DEBUFF":{
+                String[] buffLevel = str[1].split("/");
+                for (int i = 0;i!=buffLevel.length;i++){
+                    String[] buff = buffLevel[i].split(",");
+                    data.deBuffs.put(drugName+i+1,buff);//level分けようにナンバーをふる
+                }
+                break;
+            }
+        }
+        saveData(drugName,data);
+
+    }
+
+
+    static class DrugData{
+        String name;
+        int id;
+        int level;
+        int count;
+        HashMap<String,String[]> buffs = new HashMap<String, String[]>();
+        HashMap<String,String[]> deBuffs = new HashMap<String, String[]>();
+    }
+
+    public static HashMap<String,DrugData> drugMap = new HashMap<String, DrugData>();
+
+    public static DrugData loadData(String drugName){
+        DrugData data = drugMap.get(drugName);
+        if (data == null){
+            data = new DrugData();
+        }
+        return data;
+    }
+    public static DrugData saveData(String drugName,DrugData key){
+        return drugMap.put(drugName,key);
+    }
+}
--- src/main/java/red/man10/man10drugplugin/Man10DrugPlugin.java
+++ src/main/java/red/man10/man10drugplugin/Man10DrugPlugin.java
@@ -1,6 +1,12 @@
 package red.man10.man10drugplugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.*;
+import java.util.List;
+
+import static red.man10.man10drugplugin.LoadConfigData.LoadConfig;
 
 public final class Man10DrugPlugin extends JavaPlugin {
 
@@ -13,5 +19,36 @@ public final class Man10DrugPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static void drugDataLoad(){
+        File drugFolder = new File(Bukkit.getServer()
+                .getPluginManager().getPlugin("Man10DrugPlugin").getDataFolder(),File.separator);
+        if (!drugFolder.exists()){
+            Bukkit.getLogger().info("ドラッグデータを保存するフォルダが見つかりません");
+            Bukkit.getLogger().info("読み込み処理を終了します");
+            return;
+        }
+        File[] drugData = drugFolder.listFiles();
+        if (drugData == null){
+            Bukkit.getLogger().info("ドラックデータが見つかりません");
+            Bukkit.getLogger().info("読み込みを終了します");
+            return;
+        }
+        try {
+            for (int i = 0; i!=drugData.length;i++){
+                Bukkit.getLogger().info("loading..."+drugData[i].getName());
+                BufferedReader br = new BufferedReader(new FileReader(drugData[i]));
+                String str;
+                while ((str = br.readLine()) !=null){
+                    LoadConfig(drugData[i].getName().replace(".txt",""),str);
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            Bukkit.getLogger().info("catch,br");
+        } catch (IOException e) {
+            Bukkit.getLogger().info("catch,br line");
+        }
     }
 }
